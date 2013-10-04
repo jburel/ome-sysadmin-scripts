8388ae7dd2fe1fef66b323311782327723d733a2
Simon Li <spli@dundee.ac.uk>
2013-10-04 13:56:40 +0100

    Use MOCK_CFG variable

--- centos-fedora-rpms/download_and_mock_rebuild.bash
+++ centos-fedora-rpms/download_and_mock_rebuild.bash
@@ -6,6 +6,7 @@ BASE_DIR=build
 RPMS_DIR=$BASE_DIR/RPMS/x86_64
 DEBUG_DIR=$BASE_DIR/DEBUG
 SRPMS_DIR=$BASE_DIR/SRPMS
+MOCK_CFG=ome-6-x86_64
 
 download_and_build() {
 	PACKAGE=$1
@@ -20,17 +21,17 @@ download_and_build() {
 	pushd $PACKAGE
 	yumdownloader -c ../yum.conf --disablerepo=\* --enablerepo=fedora\* \
 		--source $PACKAGE
-	mock -r ome-6-x86_64 rebuild $PACKAGE-*.src.rpm $@
+	mock -r $MOCK_CFG rebuild $PACKAGE-*.src.rpm $@
 	popd
 
-	mv /var/lib/mock/ome-6-x86_64/result/$PACKAGE-*.src.rpm $SRPMS_DIR
-	mv /var/lib/mock/ome-6-x86_64/result/$PACKAGE-debuginfo-*.rpm $DEBUG_DIR
-	mv /var/lib/mock/ome-6-x86_64/result/$PACKAGE-*.rpm $RPMS_DIR
+	mv /var/lib/mock/$MOCK_CFG/result/$PACKAGE-*.src.rpm $SRPMS_DIR
+	mv /var/lib/mock/$MOCK_CFG/result/$PACKAGE-debuginfo-*.rpm $DEBUG_DIR
+	mv /var/lib/mock/$MOCK_CFG/result/$PACKAGE-*.rpm $RPMS_DIR
 }
 
-if [ ! -f /etc/mock/ome-6-x86_64.cfg ]; then
-	echo "Missing mock config: /etc/mock/ome-6-x86_64.cfg"
-	echo "Download from https://github.com/manics/centos-rpms/blob/master/mock-configs/ome-6-x86_64.cfg"
+if [ ! -f /etc/mock/$MOCK_CFG.cfg ]; then
+	echo "Missing mock config: /etc/mock/$MOCK_CFG.cfg"
+	echo "Maybe download from https://github.com/manics/centos-rpms/blob/master/mock-configs/"
 	exit 1
 fi
 
