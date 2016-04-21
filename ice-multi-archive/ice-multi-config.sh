c4a87f98490bb6466497b37ba5b7a6360be5c63b
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-04-21 16:15:03 +0100

    Remove slice path check

--- ice-multi-archive/ice-multi-config.sh
+++ ice-multi-archive/ice-multi-config.sh
@@ -67,7 +67,6 @@ omero_env() {
         prepend_path LD_LIBRARY_PATH "$ICE_HOME/lib64"
         prepend_path LIBPATH "$ICE_HOME/lib64"
         prepend_path PYTHONPATH "$ICE_HOME/python"
-        #prepend_path SLICEPATH "$ICE_HOME/slice"
     fi
 
     return 0
